576c13041cccc8b6d9e0b63b744524116d634a5c
osbo <osbo@mit.edu>
2025-09-15 00:16:05 -0400

    radix sort some more progress

--- Assets/Scripts/FluidSimulator.cs
+++ Assets/Scripts/FluidSimulator.cs
@@ -1,195 +1,250 @@
 using UnityEngine;
-using System;
 
 public class FluidSimulator : MonoBehaviour
 {
-    [SerializeField] private BoxCollider simulationBounds;
-    [SerializeField] private GameObject fluidInitialBounds;
-    
-    // Particle system parameters
-    private const int PARTICLE_COUNT = 50000;
-    
-    // Compute shader and kernel
-    [SerializeField] private ComputeShader fluidKernels;
-    [SerializeField] private ComputeShader radixSortShader;
-    private int initializeParticlesKernel;
-    
-    // GPU Buffers
-    private ComputeBuffer particlesBuffer;
-    private ComputeBuffer mortonCodesBuffer;
-    private ComputeBuffer particleIndicesBuffer;
-    private ComputeBuffer nodeFlagsBuffer; // Packed: 00(unique)(active)
-    
-    // Particle struct (must match compute shader)
-    private struct Particle
-    {
-        public Vector3 position;    // 12 bytes
-        public Vector3 velocity;    // 12 bytes
-        public uint layer;          // 4 bytes
-        public uint mortonCode;     // 4 bytes
-    }
-    
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public ComputeShader radixSortShader;
+    public ComputeShader fluidKernelsShader;
+    public int numParticles = 10000;
+    private RadixSort radixSort;
+    private ComputeBuffer mortonCodes;
+    private ComputeBuffer particleIndices;
+    private ComputeBuffer sortedMortonCodes;
+    private ComputeBuffer sortedParticleIndices;
+
     void Start()
     {
-        InitializeParticleSystem();
-    }
-    
-    private void InitializeParticleSystem()
-    {
-        // Get kernel indices
-        initializeParticlesKernel = fluidKernels.FindKernel("InitializeParticles");
-        
-        // Create buffers
-        particlesBuffer = new ComputeBuffer(PARTICLE_COUNT, sizeof(float) * 3 + sizeof(float) * 3 + sizeof(uint) + sizeof(uint)); // 32 bytes
-        mortonCodesBuffer = new ComputeBuffer(PARTICLE_COUNT, sizeof(uint));
-        particleIndicesBuffer = new ComputeBuffer(PARTICLE_COUNT, sizeof(uint));
-        nodeFlagsBuffer = new ComputeBuffer(PARTICLE_COUNT, sizeof(uint)); // 4 bytes (packed flags)
-        
-        // Set buffer data to compute shader
-        fluidKernels.SetBuffer(initializeParticlesKernel, "particlesBuffer", particlesBuffer);
-        fluidKernels.SetBuffer(initializeParticlesKernel, "mortonCodesBuffer", mortonCodesBuffer);
-        fluidKernels.SetBuffer(initializeParticlesKernel, "particleIndicesBuffer", particleIndicesBuffer);
-        fluidKernels.SetBuffer(initializeParticlesKernel, "nodeFlagsBuffer", nodeFlagsBuffer);
-        
-        // Calculate bounds
-        Vector3 simulationBoundsMin = simulationBounds.bounds.min;
-        Vector3 simulationBoundsMax = simulationBounds.bounds.max;
-        
-        // Get fluid initial bounds from the GameObject's transform
-        Vector3 fluidInitialBoundsMin = fluidInitialBounds.transform.position - fluidInitialBounds.transform.localScale * 0.5f;
-        Vector3 fluidInitialBoundsMax = fluidInitialBounds.transform.position + fluidInitialBounds.transform.localScale * 0.5f;
-        
-        // Calculate morton code normalization factors on CPU
-        Vector3 simulationSize = simulationBoundsMax - simulationBoundsMin;
-        
-        // Find the longest axis of simulation bounds
-        float maxAxis = Mathf.Max(simulationSize.x, Mathf.Max(simulationSize.y, simulationSize.z));
-        
-        // Calculate normalization factor (2^21 for longest axis)
-        float normalizationFactor = Mathf.Pow(2.0f, 21.0f) / maxAxis;
-        Vector3 mortonNormalizationFactor = new Vector3(normalizationFactor, normalizationFactor, normalizationFactor);
-        
-        // Calculate max morton value
-        float mortonMaxValue = Mathf.Pow(2.0f, 21.0f) - 1.0f;
-        
-        // Calculate grid dimensions for even particle distribution
-        Vector3 fluidInitialSize = fluidInitialBoundsMax - fluidInitialBoundsMin;
-        
-        // Calculate grid dimensions that will fit PARTICLE_COUNT particles
-        // Use the same major order as morton code (Z, Y, X)
-        float volumePerParticle = (fluidInitialSize.x * fluidInitialSize.y * fluidInitialSize.z) / PARTICLE_COUNT;
-        float gridSpacing = Mathf.Pow(volumePerParticle, 1.0f / 3.0f);
-        
-        Vector3Int gridDimensions = new Vector3Int(
-            Mathf.Max(1, Mathf.RoundToInt(fluidInitialSize.x / gridSpacing)),
-            Mathf.Max(1, Mathf.RoundToInt(fluidInitialSize.y / gridSpacing)),
-            Mathf.Max(1, Mathf.RoundToInt(fluidInitialSize.z / gridSpacing))
-        );
-        
-        // Adjust grid dimensions to ensure we don't exceed PARTICLE_COUNT
-        while (gridDimensions.x * gridDimensions.y * gridDimensions.z > PARTICLE_COUNT)
+        radixSort = new RadixSort(radixSortShader, (uint)numParticles);
+        mortonCodes = new ComputeBuffer(numParticles, sizeof(uint));
+        particleIndices = new ComputeBuffer(numParticles, sizeof(uint));
+        sortedMortonCodes = new ComputeBuffer(numParticles, sizeof(uint));
+        sortedParticleIndices = new ComputeBuffer(numParticles, sizeof(uint));
+
+        uint[] mortonCodesArray = new uint[numParticles];
+        uint[] particleIndicesArray = new uint[numParticles];
+        for (uint i = 0; i < numParticles; i++)
         {
-            if (gridDimensions.x > 1) gridDimensions.x--;
-            else if (gridDimensions.y > 1) gridDimensions.y--;
-            else if (gridDimensions.z > 1) gridDimensions.z--;
-            else break;
+            mortonCodesArray[i] = (uint)Random.Range(0, int.MaxValue);
+            particleIndicesArray[i] = i;
         }
-        
-        // Calculate actual grid spacing based on final dimensions
-        Vector3 actualGridSpacing = new Vector3(
-            fluidInitialSize.x / Mathf.Max(1, gridDimensions.x - 1),
-            fluidInitialSize.y / Mathf.Max(1, gridDimensions.y - 1),
-            fluidInitialSize.z / Mathf.Max(1, gridDimensions.z - 1)
-        );
-        
-        // Set bounds parameters to compute shader
-        fluidKernels.SetVector("simulationBoundsMin", simulationBoundsMin);
-        fluidKernels.SetVector("simulationBoundsMax", simulationBoundsMax);
-        fluidKernels.SetVector("fluidInitialBoundsMin", fluidInitialBoundsMin);
-        fluidKernels.SetVector("fluidInitialBoundsMax", fluidInitialBoundsMax);
-        fluidKernels.SetVector("mortonNormalizationFactor", mortonNormalizationFactor);
-        fluidKernels.SetFloat("mortonMaxValue", mortonMaxValue);
-        
-        // Set grid parameters to compute shader
-        fluidKernels.SetInts("gridDimensions", new int[] { (int)gridDimensions.x, (int)gridDimensions.y, (int)gridDimensions.z });
-        fluidKernels.SetVector("gridSpacing", actualGridSpacing);
-        
-        // Dispatch the kernel
-        int threadGroups = Mathf.CeilToInt(PARTICLE_COUNT / 64.0f);
-        fluidKernels.Dispatch(initializeParticlesKernel, threadGroups, 1, 1);
+
+        mortonCodes.SetData(mortonCodesArray);
+        particleIndices.SetData(particleIndicesArray);
+
+        radixSort.Sort(mortonCodes, particleIndices, sortedMortonCodes, sortedParticleIndices, (uint)numParticles);
+
+        uint[] sortedMortonCodesArray = new uint[numParticles];
+        sortedMortonCodes.GetData(sortedMortonCodesArray);
+
+        string sorted_output = "Sorted Morton Codes (first 100): ";
+        for (int i = 0; i < Mathf.Min(100, numParticles); i++)
+        {
+            sorted_output += sortedMortonCodesArray[i] + " ";
+        }
+        Debug.Log(sorted_output);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDestroy()
     {
-        
+        radixSort.ReleaseBuffers();
+        mortonCodes.Release();
+        particleIndices.Release();
+        sortedMortonCodes.Release();
+        sortedParticleIndices.Release();
     }
-    
-    // Simple debug visualization using Gizmos
-    private void OnDrawGizmos()
-    {
-        if (particlesBuffer == null) return;
-        
-        // Read particle data back to CPU for visualization
-        Particle[] particles = new Particle[PARTICLE_COUNT];
-        particlesBuffer.GetData(particles);
-        
-        // Set gizmo color to blue
-        Gizmos.color = Color.blue;
-        
-        // Draw each particle as a small sphere
-        for (int i = 0; i < PARTICLE_COUNT; i++)
-        {
-            float size = Mathf.Pow(8.0f, particles[i].layer) * 0.002f; // Scale down the size
-            Gizmos.DrawSphere(particles[i].position, size);
-        }
-    }
-    
-    private void ReadAndPrintBufferData()
-    {
-        const int elementsToRead = 10;
-        
-        // Read particles buffer
-        Particle[] particles = new Particle[elementsToRead];
-        particlesBuffer.GetData(particles, 0, 0, elementsToRead);
-        
-        // Read morton codes buffer
-        uint[] mortonCodes = new uint[elementsToRead];
-        mortonCodesBuffer.GetData(mortonCodes, 0, 0, elementsToRead);
+}
 
-        // Read particle indices buffer
-        uint[] particleIndices = new uint[elementsToRead];
-        particleIndicesBuffer.GetData(particleIndices, 0, 0, elementsToRead);
+public class RadixSort
+{
+    private ComputeShader sortShader;
+    private int prefixSumKernel;
+    private int prefixFixupKernel;
+    private int splitPrepKernel;
+    private int splitScatterKernel;
+    private int copyBufferKernel;
+    private int copyIndicesKernel;
+    private int clearBuffer32Kernel;
 
-        // Read node flags buffer (packed: 00(unique)(active))
-        uint[] nodeFlags = new uint[elementsToRead];
-        nodeFlagsBuffer.GetData(nodeFlags, 0, 0, elementsToRead);
-        
-        // Print buffer data
-        Debug.Log("=== Buffer Data (First 10 Elements) ===");
-        
-        for (int i = 0; i < elementsToRead; i++)
+    private ComputeBuffer tempKeys;
+    private ComputeBuffer tempIndices;
+    private ComputeBuffer tempKeysB;
+    private ComputeBuffer tempIndicesB;
+    private ComputeBuffer auxBuffer;
+    private ComputeBuffer aux2Buffer;
+    private ComputeBuffer auxSmallBuffer;
+    private ComputeBuffer eBuffer;
+    private ComputeBuffer fBuffer;
+
+    private uint maxLength;
+
+    public RadixSort(ComputeShader shader, uint maxLength)
+    {
+        this.maxLength = maxLength;
+        this.sortShader = shader;
+
+        prefixSumKernel = sortShader.FindKernel("prefixSum");
+        prefixFixupKernel = sortShader.FindKernel("prefixFixup");
+        splitPrepKernel = sortShader.FindKernel("split_prep");
+        splitScatterKernel = sortShader.FindKernel("split_scatter");
+        copyBufferKernel = sortShader.FindKernel("copyBuffer");
+        copyIndicesKernel = sortShader.FindKernel("copyIndices");
+        clearBuffer32Kernel = sortShader.FindKernel("clearBuffer32");
+
+        tempKeys = new ComputeBuffer((int)maxLength, sizeof(uint), ComputeBufferType.Default);
+        tempIndices = new ComputeBuffer((int)maxLength, sizeof(uint), ComputeBufferType.Default);
+        tempKeysB = new ComputeBuffer((int)maxLength, sizeof(uint), ComputeBufferType.Default);
+        tempIndicesB = new ComputeBuffer((int)maxLength, sizeof(uint), ComputeBufferType.Default);
+        eBuffer = new ComputeBuffer((int)maxLength, sizeof(uint), ComputeBufferType.Default);
+        fBuffer = new ComputeBuffer((int)maxLength, sizeof(uint), ComputeBufferType.Default);
+
+        uint threadgroupSize = 512;
+        uint numThreadgroups = (maxLength + (threadgroupSize * 2) - 1) / (threadgroupSize * 2);
+        uint requiredAuxSize = System.Math.Max(1, numThreadgroups);
+        auxBuffer = new ComputeBuffer((int)requiredAuxSize, sizeof(uint), ComputeBufferType.Default);
+        aux2Buffer = new ComputeBuffer((int)requiredAuxSize, sizeof(uint), ComputeBufferType.Default);
+        auxSmallBuffer = new ComputeBuffer(1, sizeof(uint), ComputeBufferType.Default);
+    }
+
+    public void ReleaseBuffers()
+    {
+        tempKeys.Release();
+        tempIndices.Release();
+        tempKeysB.Release();
+        tempIndicesB.Release();
+        auxBuffer.Release();
+        aux2Buffer.Release();
+        auxSmallBuffer.Release();
+        eBuffer.Release();
+        fBuffer.Release();
+    }
+
+    public void Sort(ComputeBuffer inputKeys, ComputeBuffer inputIndices, ComputeBuffer outputKeys, ComputeBuffer outputIndices, uint actualCount)
+    {
+        if (actualCount == 0) return;
+
+        ClearBuffer(tempKeys, (uint)tempKeys.count);
+        ClearBuffer(tempIndices, (uint)tempIndices.count);
+        ClearBuffer(tempKeysB, (uint)tempKeysB.count);
+        ClearBuffer(tempIndicesB, (uint)tempIndicesB.count);
+
+        int threadGroupSize = 512;
+        int threadGroups = (int)((actualCount + threadGroupSize - 1) / threadGroupSize);
+
+        sortShader.SetBuffer(copyBufferKernel, "input", inputKeys);
+        sortShader.SetBuffer(copyBufferKernel, "output", tempKeys);
+        sortShader.SetInt("count", (int)actualCount);
+        sortShader.Dispatch(copyBufferKernel, threadGroups, 1, 1);
+
+        sortShader.SetBuffer(copyIndicesKernel, "inputIndices", inputIndices);
+        sortShader.SetBuffer(copyIndicesKernel, "outputIndices", tempIndices);
+        sortShader.SetInt("count", (int)actualCount);
+        sortShader.Dispatch(copyIndicesKernel, threadGroups, 1, 1);
+
+        ComputeBuffer keysIn = tempKeys;
+        ComputeBuffer keysOut = tempKeysB;
+        ComputeBuffer indicesIn = tempIndices;
+        ComputeBuffer indicesOut = tempIndicesB;
+
+        for (int i = 0; i < 32; i++)
         {
-            Debug.Log($"Index {i}:");
-            Debug.Log($"  Particle: pos=({particles[i].position.x:F3}, {particles[i].position.y:F3}, {particles[i].position.z:F3}), " +
-                     $"vel=({particles[i].velocity.x:F3}, {particles[i].velocity.y:F3}, {particles[i].velocity.z:F3}), " +
-                     $"layer={particles[i].layer}, mortonCode={particles[i].mortonCode}");
-            Debug.Log($"  Morton Code: {mortonCodes[i]}, Particle Index: {particleIndices[i]}");
-            // Unpack bit flags: 00(unique)(active)
-            bool isActive = (nodeFlags[i] & 0x1) != 0;      // Bit 0: active
-            bool isUnique = (nodeFlags[i] & 0x2) != 0;      // Bit 1: unique
-            Debug.Log($"  NodeFlags: {nodeFlags[i]} (Active: {isActive}, Unique: {isUnique})");
+            EncodeSplit(keysIn, indicesIn, keysOut, indicesOut, (uint)i, actualCount);
+            (keysIn, keysOut) = (keysOut, keysIn);
+            (indicesIn, indicesOut) = (indicesOut, indicesIn);
         }
+
+        sortShader.SetBuffer(copyBufferKernel, "input", keysIn);
+        sortShader.SetBuffer(copyBufferKernel, "output", outputKeys);
+        sortShader.SetInt("count", (int)actualCount);
+        sortShader.Dispatch(copyBufferKernel, threadGroups, 1, 1);
+
+        sortShader.SetBuffer(copyIndicesKernel, "inputIndices", indicesIn);
+        sortShader.SetBuffer(copyIndicesKernel, "outputIndices", outputIndices);
+        sortShader.SetInt("count", (int)actualCount);
+        sortShader.Dispatch(copyIndicesKernel, threadGroups, 1, 1);
+    }
+
+    private void EncodeSplit(ComputeBuffer input, ComputeBuffer inputIndices, ComputeBuffer output, ComputeBuffer outputIndices, uint bit, uint count)
+    {
+        sortShader.SetBuffer(splitPrepKernel, "input", input);
+        sortShader.SetInt("bit", (int)bit);
+        sortShader.SetBuffer(splitPrepKernel, "e", eBuffer);
+        sortShader.SetInt("count", (int)count);
+        int threadGroups = (int)((count + 512 - 1) / 512);
+        sortShader.Dispatch(splitPrepKernel, threadGroups, 1, 1);
+
+        EncodeScan(eBuffer, fBuffer, count, bit);
+
+        sortShader.SetBuffer(splitScatterKernel, "input", input);
+        sortShader.SetBuffer(splitScatterKernel, "inputIndices", inputIndices);
+        sortShader.SetBuffer(splitScatterKernel, "output", output);
+        sortShader.SetBuffer(splitScatterKernel, "outputIndices", outputIndices);
+        sortShader.SetInt("bit", (int)bit);
+        sortShader.SetBuffer(splitScatterKernel, "e", eBuffer);
+        sortShader.SetBuffer(splitScatterKernel, "f", fBuffer);
+        sortShader.SetInt("count", (int)count);
+        sortShader.Dispatch(splitScatterKernel, threadGroups, 1, 1);
+    }
+
+    private void EncodeScan(ComputeBuffer input, ComputeBuffer output, uint length, uint bit)
+    {
+        if (length == 0) return;
+
+        uint threadgroupSize = 512;
+        uint numThreadgroups = (length + (threadgroupSize * 2) - 1) / (threadgroupSize * 2);
+        uint zeroff = 1;
+
+        if (numThreadgroups <= 1)
+        {
+            sortShader.SetBuffer(prefixSumKernel, "input", input);
+            sortShader.SetBuffer(prefixSumKernel, "output", output);
+            sortShader.SetBuffer(prefixSumKernel, "aux", auxBuffer);
+            sortShader.SetInt("len", (int)length);
+            sortShader.SetInt("zeroff", (int)zeroff);
+            sortShader.Dispatch(prefixSumKernel, 1, 1, 1);
+        }
+        else
+        {
+            sortShader.SetBuffer(prefixSumKernel, "input", input);
+            sortShader.SetBuffer(prefixSumKernel, "output", output);
+            sortShader.SetBuffer(prefixSumKernel, "aux", auxBuffer);
+            sortShader.SetInt("len", (int)length);
+            sortShader.SetInt("zeroff", (int)zeroff);
+            sortShader.Dispatch(prefixSumKernel, (int)numThreadgroups, 1, 1);
+
+            uint auxLength = numThreadgroups;
+            sortShader.SetBuffer(prefixSumKernel, "input", auxBuffer);
+            sortShader.SetBuffer(prefixSumKernel, "output", aux2Buffer);
+            sortShader.SetBuffer(prefixSumKernel, "aux", auxSmallBuffer);
+            sortShader.SetInt("len", (int)auxLength);
+            sortShader.SetInt("zeroff", (int)zeroff);
+            sortShader.Dispatch(prefixSumKernel, 1, 1, 1);
+
+            sortShader.SetBuffer(prefixFixupKernel, "input", output);
+            sortShader.SetBuffer(prefixFixupKernel, "aux", aux2Buffer);
+            sortShader.SetInt("len", (int)length);
+            sortShader.Dispatch(prefixFixupKernel, (int)numThreadgroups, 1, 1);
+        }
+        VerifyPrefixSum(output, length, bit);
     }
     
-    private void OnDestroy()
+    private void VerifyPrefixSum(ComputeBuffer prefixSumBuffer, uint N, uint bit)
     {
-        // Clean up buffers
-        particlesBuffer?.Release();
-        mortonCodesBuffer?.Release();
-        particleIndicesBuffer?.Release();
-        nodeFlagsBuffer?.Release();
+        uint[] data = new uint[N];
+        prefixSumBuffer.GetData(data);
+
+        Debug.Log($"Prefix Sum Verification for bit {bit}:");
+        string output = "Prefix Sum (first 100): ";
+        for (int i = 0; i < Mathf.Min(100, N); i++)
+        {
+            output += data[i] + " ";
+        }
+        Debug.Log(output);
+    }
+
+
+    private void ClearBuffer(ComputeBuffer buffer, uint count)
+    {
+        sortShader.SetBuffer(clearBuffer32Kernel, "output", buffer);
+        int threadGroups = (int)((count + 63) / 64);
+        sortShader.Dispatch(clearBuffer32Kernel, threadGroups, 1, 1);
     }
 }
